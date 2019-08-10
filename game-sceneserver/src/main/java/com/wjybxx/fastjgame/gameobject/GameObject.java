8548d79e57e45ad0a75f7a07812216fed3b34e7d
wjybxx <845740757@qq.com>
2019-08-10 21:44:25 +0800

    今天主要修复了网络层的时序bug。 优化是bug之源

--- game-sceneserver/src/main/java/com/wjybxx/fastjgame/gameobject/GameObject.java
+++ game-sceneserver/src/main/java/com/wjybxx/fastjgame/gameobject/GameObject.java
@@ -17,9 +17,11 @@
 package com.wjybxx.fastjgame.gameobject;
 
 import com.wjybxx.fastjgame.misc.ViewGrid;
+import com.wjybxx.fastjgame.scene.Scene;
 import com.wjybxx.fastjgame.scene.gameobjectdata.GameObjectData;
 import com.wjybxx.fastjgame.scene.gameobjectdata.GameObjectType;
 import com.wjybxx.fastjgame.shape.Point2D;
+import com.wjybxx.fastjgame.trigger.*;
 
 import javax.annotation.Nonnull;
 
@@ -32,17 +34,26 @@ import javax.annotation.Nonnull;
  */
 public abstract class GameObject<T extends GameObjectData> {
 
+    /** 对象当前所在的场景 */
+    protected Scene scene;
+    /** 对象绑定的定时器 */
+    private final TimerSystem timerSystem;
     /**
      * 游戏对象的坐标
      * 先写波2D的，练手AOI
      */
-    private final Point2D position=Point2D.newPoint2D();
+    private final Point2D position = Point2D.newPoint2D();
     /**
      * 对象当前所在的视野格子;
      * 缓存的视野格子，用于降低视野更新频率；
      * 游戏对象进入视野时需要立即初始化，离开后需要删除；
      */
     private ViewGrid viewGrid;
+
+    public GameObject(Scene scene, SystemTimeProvider timeProvider) {
+        this.scene = scene;
+        this.timerSystem = new DefaultTimerSystem(timeProvider);
+    }
 
     public Point2D getPosition() {
         return position;
@@ -75,4 +86,37 @@ public abstract class GameObject<T extends GameObjectData> {
      */
     @Nonnull
     public abstract T getData();
+
+    public void tick() {
+        timerSystem.tick();
+    }
+
+    @Nonnull
+    public TimeoutHandle newTimeout(long timeout, @Nonnull TimerTask<TimeoutHandle> task) {
+        return timerSystem.newTimeout(timeout, task);
+    }
+
+    public TimeoutHandle nextTick(@Nonnull TimerTask<TimeoutHandle> task) {
+        return timerSystem.nextTick(task);
+    }
+
+    @Nonnull
+    public FixedDelayHandle newFixedDelay(long initialDelay, long delay, @Nonnull TimerTask<FixedDelayHandle> task) {
+        return timerSystem.newFixedDelay(initialDelay, delay, task);
+    }
+
+    @Nonnull
+    public FixedDelayHandle newFixedDelay(long delay, @Nonnull TimerTask<FixedDelayHandle> task) {
+        return timerSystem.newFixedDelay(delay, task);
+    }
+
+    @Nonnull
+    public FixedRateHandle newFixRate(long initialDelay, long period, @Nonnull TimerTask<FixedRateHandle> task) {
+        return timerSystem.newFixRate(initialDelay, period, task);
+    }
+
+    @Nonnull
+    public FixedRateHandle newFixRate(long period, @Nonnull TimerTask<FixedRateHandle> task) {
+        return timerSystem.newFixRate(period, task);
+    }
 }
