138469f2e178192bce18bd97e24e3f40f1871260
wjybxx <845740757@qq.com>
2020-03-10 23:59:35 +0800

    ...

--- game-utils/src/main/java/com/wjybxx/fastjgame/utils/concurrent/CompleteFuture.java
+++ game-utils/src/main/java/com/wjybxx/fastjgame/utils/concurrent/CompleteFuture.java
@@ -48,6 +48,11 @@ public abstract class CompleteFuture<V> implements BlockingFuture<V> {
     }
 
     @Override
+    public final boolean isVoid() {
+        return false;
+    }
+
+    @Override
     public final boolean isDone() {
         return true;
     }
