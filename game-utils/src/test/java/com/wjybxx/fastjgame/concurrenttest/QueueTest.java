09b7f57b6814943089a480e3feed6f708f86e6ff
wjybxx <845740757@qq.com>
2020-05-13 11:26:46 +0800

    更新测试用例

--- game-utils/src/test/java/com/wjybxx/fastjgame/concurrenttest/QueueTest.java
+++ game-utils/src/test/java/com/wjybxx/fastjgame/concurrenttest/QueueTest.java
@@ -30,12 +30,12 @@ import java.util.concurrent.CountDownLatch;
 import java.util.stream.IntStream;
 
 /**
- * 一个简单的不靠谱测试
- * 以消费者Pool 100W个任务为基础，数据如下(单位:毫秒):
+ * 一个简单的不靠谱测试(测试的时候修改了{@link DisruptorEventLoop}和{@link TemplateEventLoop}的一些实现)
+ * 以消费者Pool消费100W个任务为基础，数据如下(单位:毫秒):
  * MpscArrayQueue: 105-110 非常稳定
  * Disruptor:  极致110  但更多落在130~140
  * ConcurrentLinkedQueue 极致130 但更多落在140-150
- * MpscLinkedQueue：极致45~50 但被JVM反优化后更多落在300+   这个差别太大，还没较仔细的研究，猜测是JVM干了什么特殊的优化导致的....
+ * MpscLinkedQueue：极致45~50 但更多落在300+  这个差别太大，还没较仔细的研究，猜测是JVM干了什么特殊的优化导致的....(Debug模式表现稳定，极致性能，但是Run模式下不稳定)
  *
  * @author wjybxx
  * @version 1.0
@@ -46,19 +46,20 @@ public class QueueTest {
     private static volatile boolean stop = false;
 
     public static void main(String[] args) throws InterruptedException {
-        final int consumerNum = 8;
+        final int producerNum = 8;
 
         final EventLoop consumer =
                 newUnboundEventLoop();
-//                newDisruptorEventLoop(consumerNum * maxLoop);
+//                newDisruptorEventLoop(100_0000 / producerNum);
+
         // 先启动
         consumer.execute(ConcurrentUtils.NO_OP_TASK);
 
         final CountDownLatch startLatch = new CountDownLatch(1);
-        final CountDownLatch stopLatch = new CountDownLatch(consumerNum);
+        final CountDownLatch stopLatch = new CountDownLatch(producerNum);
 
-        IntStream.range(0, consumerNum)
-                .mapToObj(i -> new Worker(consumer, startLatch, stopLatch))
+        IntStream.range(0, producerNum)
+                .mapToObj(i -> new Producer(consumer, startLatch, stopLatch))
                 .forEach(Thread::start);
 
         startLatch.countDown();
@@ -76,7 +77,8 @@ public class QueueTest {
 
     private static TemplateEventLoop newUnboundEventLoop() {
         return new TemplateEventLoop(null, new DefaultThreadFactory("CONSUMER"),
-                RejectedExecutionHandlers.discard(), new YieldWaitStrategyFactory());
+                RejectedExecutionHandlers.discard(),
+                new YieldWaitStrategyFactory());
     }
 
     private static DisruptorEventLoop newDisruptorEventLoop(int minQueueSize) {
@@ -86,13 +88,13 @@ public class QueueTest {
                 new com.wjybxx.fastjgame.utils.concurrent.disruptor.YieldWaitStrategyFactory());
     }
 
-    private static class Worker extends Thread {
+    private static class Producer extends Thread {
 
         private final EventLoop consumer;
         private final CountDownLatch startLatch;
         private final CountDownLatch stopLatch;
 
-        private Worker(EventLoop consumer, CountDownLatch startLatch, CountDownLatch stopLatch) {
+        private Producer(EventLoop consumer, CountDownLatch startLatch, CountDownLatch stopLatch) {
             this.consumer = consumer;
             this.startLatch = startLatch;
             this.stopLatch = stopLatch;
@@ -107,7 +109,6 @@ public class QueueTest {
                     for (int index = 0; index < 8192; index++) {
                         consumer.execute(ConcurrentUtils.NO_OP_TASK);
                     }
-
                 } while (!stop);
 
             } catch (InterruptedException e) {
